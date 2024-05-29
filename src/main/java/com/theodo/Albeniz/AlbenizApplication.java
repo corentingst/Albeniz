ff9edbbcb7bf5169faadc2e5470eb5f31f1cd6a1
corenting <corenting@theodo.fr>
2024-05-29 12:28:18 +0200

    Initial commit

---
+++ src/main/java/com/theodo/Albeniz/AlbenizApplication.java
@@ -0,0 +1,13 @@
+package com.theodo.Albeniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbenizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbenizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theodo/Albeniz/AlbenizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theodo.Albeniz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbenizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
